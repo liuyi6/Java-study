cc7f32298e5b641d424655affb50ec09788f1c4f
liuyi6 <ly616mail@163.com>
2018-11-01 23:59:22 +0800

    冒泡排序修改

--- src/main/java/com/luis/algorithm/BubbleSort.java
+++ src/main/java/com/luis/algorithm/BubbleSort.java
@@ -20,17 +20,14 @@ public class BubbleSort {
 	public static int[] bubbleSort1(int [] arr){
 		int i, j , n= arr.length; 
 		for(i=0; i<n; i++){
-			//表示n次排序过程。 
 			for(j=1; j<n-i; j++){ 
 				if(arr[j-1] > arr[j]){
-					//前面的数字大于后面的数字就交换 
-					//交换a[j-1]和a[j] 
 					int temp = arr[j-1]; 
 					arr[j-1] = arr[j]; 
 					arr[j]=temp; 
-					} 
 				} 
-			}
+			} 
+		}
 		return arr;
 	}
 }
